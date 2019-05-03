f4758b2ccfe1eb176337485adac350139c9e8724
Mihail Paley <undeadlol1@mail.ru>
2019-05-03 12:51:25 +0500

    (problems/reducer) fix 'ADD_PROBLEM' action

--- src/reducers/problems.js
+++ src/reducers/problems.js
@@ -1,13 +1,25 @@
+import uuid from 'uuid/v4';
 import Store, { emptyProblem } from '../store/problems';
 
 export default function problemsReducer(state = Store, action) {
-  console.log('action: ', action.type);
   switch (action.type) {
+    // Add new problem into "values" array.
     case 'ADD_PROBLEM': {
-      return {
+      const newState = {
         ...state,
-        problems: state.problems.concat([action.data]),
+        values: [
+          ...state.values,
+          {
+            // Create unique id to avoid problems with:
+            // 1) Missing keys in List components.
+            // 2) Syncing with database.
+            id: uuid(),
+            ...action.data,
+          },
+        ],
+
       };
+      return newState;
     }
     default:
       return state;
