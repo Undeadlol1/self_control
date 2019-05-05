08bd0e619a8cdeb99c23b7d3ecb55b29f3a7c47d
Mihail Paley <undeadlol1@mail.ru>
2019-05-05 21:29:21 +0500

    add CreateSolutions container

---
+++ src/containers/CreateSolution.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import CreateProblem from '../native/components/Problem/Create';
+import create from '../actions/problems';
+
+class CreateSolutionContainer extends Component {
+  static propTypes = {
+    // Due to constant problems in router,
+    // passing component as "Layout" is disabled.
+    // TODO: fix or remove this line.
+    // Layout: PropTypes.func.isRequired,
+    member: PropTypes.shape({}).isRequired,
+    onFormSubmit: PropTypes.func.isRequired,
+  }
+
+  state = {
+    error: null,
+    success: null,
+    loading: false,
+  }
+
+  onFormSubmit = (data) => {
+    const { onFormSubmit } = this.props;
+
+    this.setState({ loading: true });
+
+    return onFormSubmit(data)
+      .then(() => this.setState({
+        loading: false,
+        success: 'success', // i18n trsnslation key
+        error: null,
+      })).catch((err) => {
+        this.setState({
+          loading: false,
+          success: null,
+          error: err,
+        });
+        throw err; // To prevent transition back
+      });
+  }
+
+  render = () => {
+    const { member } = this.props;
+    const { error, loading, success } = this.state;
+    return (
+      <CreateProblem
+        error={error}
+        member={member}
+        loading={loading}
+        success={success}
+        onFormSubmit={this.onFormSubmit}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  member: state.member || {},
+});
+
+const mapDispatchToProps = {
+  onFormSubmit: create,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateSolutionContainer);
