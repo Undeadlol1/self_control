410cc6ec10b4025cf8502a2c1d6ea1c92f02fc23
Mihail Paley <undeadlol1@mail.ru>
2019-05-07 17:09:13 +0500

    (actions/problems) "addSolution" added

--- src/actions/problems.js
+++ src/actions/problems.js
@@ -1,7 +1,8 @@
+import generateUuid from 'uuid/v4';
 /**
  * Create a problem.
  * @param {object} data problem object directly from form.
- * @property {string} data.name
+ * @property {string} data.title
  * @property {string} data.image Problem's image URL.
  * @returns {Promise} successful resolve function.
  */
@@ -14,3 +15,17 @@ export default function addProblem(data) {
     return resolve(dispatch({ type: 'ADD_PROBLEM', data }));
   }).catch((err) => { throw err.message; });
 }
+/**
+ * Create a solution.
+ * @param {object} data Solution object directly from form.
+ * @property {string} data.title
+ * @property {string} data.problemId Solution's parent id.
+ * @returns {Promise} successful resolve function.
+ */
+export const addSolution = data => dispatch => new Promise(async (resolve, reject) => {
+  // Validation rules
+  if (!data.title) return reject({ message: 'missingTitle' });
+
+  // Call reducer action.
+  return resolve(dispatch({ type: 'ADD_SOLUTION', data }));
+}).catch((err) => { throw err.message; });
